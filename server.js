f4c4f8e9f43f1ce94b817ac90d1adedf66e64311
arceojoshua94 <arceojoshua94@outlook.com>
2023-11-09 20:03:59 -0500

    troubleshooting

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 const express = require('express');
-const path = require('path');
-const htmlRoutes = require('./routes/htmlRoutes');
-const apiRoutes = require('./routes/apiRoutes');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const path = require('path');
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+const fs = require('fs-extra');
+const { v4: uuidv4 } = require('uuid');
+
+app.get('/api/notes', async (req, res) => {
+  const notes = await fs.readJson('./db/db.json');
+  res.json(notes);
+});
+
+app.post('/api/notes', async (req, res) => {
+  const newNote = { ...req.body, id: uuidv4() };
+  const notes = await fs.readJson('./db/db.json');
+  notes.push(newNote);
+  await fs.writeJson('./db/db.json', notes);
+  res.json(newNote);
 });
